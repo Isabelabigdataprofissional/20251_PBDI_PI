5cba27ff0069b555148402171b38d0b5df46834d
Isabelabigdataprofissional <isabelaprofissional.bigdata@gmail.com>
2025-06-23 18:25:21 -0300

    feat(pi_3): adiciona requisitos

---
+++ pi_3.sql
@@ -0,0 +1,22 @@
+-- 1 Stored procedure com cursor não vinculado de query dinâmica (PL/pgSQL)
+-- Escreva um stored procedure que usa um cursor não vinculado de query dinâmica para percorrer os registros de uma ou mais tabelas da sua base de dados e calcular uma estatística relevante para o seu domínio. 
+--A estatística pode ser, por exemplo
+-- - Média, soma, contagem ou proporção de registros com alguma condição.
+-- - Cálculo de desvio padrão, máximo, mínimo ou mediana.
+-- - Taxa de crescimento ou queda entre períodos.
+-- - Frequência de ocorrência de um valor específico.
+
+
+
+-- 2 Trigger (PL/pgSQL)
+-- Crie um trigger (e sua função associada) que execute uma ação automática sempre que um evento relevante ocorrer em uma ou mais tabelas da sua base de dados. 
+--O trigger pode ser definido para responder a eventos como
+-- - Inserção de novos registros
+-- - Atualização de campos críticos
+-- - Remoção de dados importantes
+-- Exemplos de ações que podem ser implementadas
+-- - Registro automático de logs em uma tabela auxiliar.
+-- - Atualização de dados agregados em outra tabela.
+-- - Envio de alerta ou bloqueio de alterações específicas.
+
+
